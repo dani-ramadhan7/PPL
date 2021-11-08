0c538ce2f221d151dbb993c21d4ca96813082ec8
Ramadhan Arif H <dani.ramadhan7@gmail.com>
2021-11-08 16:34:38 +0700

    Update AnimalFarm.py

--- AnimalFarm.py
+++ AnimalFarm.py
@@ -1,14 +1,42 @@
-class HewanTernak:
-    def __init__(self, name, jenisHewan, jenisMakanan):
+class Animal:
+    def __init__(self, name, type, FoodType):
         self.name = name
-        self.jenisHewan = jenisHewan
-        self.jenisMakanan = jenisMakanan
+        self.type = type
+        self.FoodType = FoodType
     
     def printDescription(self):
-        print(self.name, self.jenisHewan, self.jenisMakanan)
+        print(self.name + " is a type of " + self.type + " and has a type of food " + self.FoodType)
 
-Ayam = HewanTernak("Ayam", "unggas", "omnivora")
-Kambing = HewanTernak("Kambing", "mamalia", "herbivora")
+    def eat(self, food):
+        print(self.name + " is eating " + food)
 
-Ayam.printDescription()
-Kambing.printDescription()
+    def sleep(self):
+        print(self.name + " is sleeping")
+
+class Chicken(Animal):
+    def sound(self):
+        print(self.name + " : " + "Cook-a-doodle-doo!")
+
+class Sheep(Animal):
+    def sound(self):
+        print(self.name + " : " + "baa.. baa..")
+
+Chicken1 = Animal("Chicken1", "Aves", "Omnivores")
+Sheep1 = Animal("Sheep1", "Mammal", "Herbivores")
+
+Chicken1.printDescription()
+Chicken1.sleep()
+Chicken1.eat("grains")
+
+Sheep1.printDescription()
+Sheep1.sleep()
+Sheep1.eat("grass")
+
+Chicken2 = Chicken("Chicken2", "Aves", "Omnivores")
+Chicken2.sound();
+
+Sheep2 = Sheep("Sheep2", "Mammal", "Herbivores")
+Sheep2.sound();
+# Chicken2.printDescription()
+# Chicken2.sleep()
+# Chicken2.eat("grains") 
